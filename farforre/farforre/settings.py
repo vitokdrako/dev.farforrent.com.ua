981c0d77561a1afad27c63b08925d10444c495e6
Vitokdrako <vitokdrako@gmail.com>
2023-11-29 22:33:39 +0100

    language

--- farforre/farforre/settings.py
+++ farforre/farforre/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'farforre.urls'
@@ -112,9 +113,16 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
+LANGUAGES = [
+    ('en', 'English'),
+    ('uk', 'Українська'),
+]
+
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
--- farforre/farforre/urls.py
+++ farforre/farforre/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.i18n import i18n_patterns
 from . import views
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
     path('', views.index, name='index'),
-]
+    # Додайте інші URL-шляхи тут, якщо вони у вас є
+)
+
