90672e848b398222e18372047be9e0d28794cfc0
Vitokdrako <vitokdrako@gmail.com>
2023-11-29 18:43:01 +0100

    html add

--- farforre/farforre/settings.py
+++ farforre/farforre/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'farforre.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'farforre' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
